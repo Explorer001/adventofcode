18aa4e4cb028e725492547750b930d2c4f9e1c79
Explorer001 <lukas.gehreke@gmail.com>
2017-12-07 22:56:02 +0100

    added method for part 2

--- 2017/day7/day7.rs
+++ 2017/day7/day7.rs
@@ -8,10 +8,72 @@ fn main() {
                                             .map(|s| s.split_whitespace()
                                                       .filter(|&s| s != "->")
                                                       .map(|s| s.replace(",","")
+                                                                .replace("(","")
+                                                                .replace(")","")
                                                                 .to_string())
                                                       .collect())
                                             .collect();
-    
+    //println!("{:?}", input);
+
+    let root = get_root(input.to_vec());
+    println!("Root: {}", root);
+    let foo = get_unbalanced(input.to_vec(), root.to_string());
+    println!("{}", foo);
+}
+
+fn get_unbalanced(input: Vec<Vec<String>>, node: String) -> i32{
+    for i in 0..input.len() {
+        if input[i][0] == node {
+            if input[i].len() == 2 {
+                //println!("{:?}", input[i]);
+                return input[i][1].parse().unwrap();
+            } else {
+                let mut sol: Vec<i32> = Vec::new(); 
+                for j in 2..input[i].len() {
+                    sol.push(get_unbalanced(input.to_vec(), input[i][j].to_string()));
+                }
+                //println!("{:?} ||| {:?}", input[i], sol);
+                let mut unbal = false;
+                for k in 0..sol.len()-1 {
+                    if sol[k] != sol[k+1]  {
+                        unbal = true;
+                        break;
+                    }
+                }
+                if unbal {
+                    let mut max_ind = 0;
+                    let mut max = 0;
+                    for i in 0..sol.len() {
+                        if sol[i] > max {
+                            max = sol[i];
+                            max_ind = i;
+                        }
+                    }
+                    let unbal_program = input[i][max_ind + 2].to_string();
+                    let mut unbal_weight = 0;
+                    for g in 0..input.len() {
+                        if input[g][0] == unbal_program {
+                            unbal_weight = input[g][1].parse::<i32>().unwrap();
+                            break;
+                        }
+                    }
+                    println!("--------------------------------------");
+                    println!("{:?}", sol);
+                    println!("{} disk is not balanced", input[i][0]);
+                    println!("{:?}", input[i]);
+                    let max: &i32 = sol.iter().max().unwrap();
+                    let min: &i32 = sol.iter().min().unwrap();
+                    println!("{} weight must be {}", unbal_program, unbal_weight - (*max - *min));
+                    println!("--------------------------------------");
+                }
+                return input[i][1].parse::<i32>().unwrap() + sol.iter().sum::<i32>();
+            }
+        }
+    }
+    return 0;
+}
+
+fn get_root(input: Vec<Vec<String>>) -> String {
     let mut elementi: Vec<String>;
     let mut elementj: Vec<String>;
     let mut name: String = "foo".to_string();
@@ -44,6 +106,5 @@ fn main() {
         }
     }
 
-    println!("{}", name);
-                         
+    name                        
 }
