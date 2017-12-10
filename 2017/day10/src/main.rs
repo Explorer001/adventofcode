1ee5c67aedb3fb291e995bc6e52df3454bd65cc4
Explorer001 <lukas.gehreke@gmail.com>
2017-12-10 20:24:16 +0100

    done. going to bed now. doesnt work for part 1 anymore.

--- 2017/day10/src/main.rs
+++ 2017/day10/src/main.rs
@@ -7,25 +7,14 @@ fn main() {
                                            .map(|s| s.to_string())
                                            .collect();
 
-    let input: Vec<i32> = in_str.split(",")
-                               .map(|s| s.parse()
-                                         .unwrap())
-                               .collect();
+    println!("{}", hash(in_str.to_string(), 64));
 
-    let mut ascii_in: Vec<i32> = in_str.chars()
-                                       .map(|c| (c as u8) as i32)
-                                       .collect();
+    //println!("{:?}", hash("1,2,3".to_string(), 64));
 
-    let extension: Vec<i32> = vec![17, 31, 73, 47, 23];
-
-    ascii_in.extend(extension.iter().cloned());
-
-    println!("{:?}", ascii_in);
-
-    println!("{}", hash(input.to_vec(), 1));
+    //println!("{}", xor_part(vec![65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]));
 }
 
-fn hash(length_vector: Vec<i32>, rounds: i32) -> i32{
+fn hash(in_str: String, range: i32) -> String{
     let mut position: i32 = 0;
     let mut skip_size: i32 = 0;
     let mut temp: i32;
@@ -33,14 +22,27 @@ fn hash(length_vector: Vec<i32>, rounds: i32) -> i32{
     let mut temp_begin: i32;
     let mut temp_end: i32;
 
+    let mut length_vector: Vec<i32> = in_str.chars()
+                                            .map(|c| (c as u8) as i32)
+                                            .collect();
+
+    let extension: Vec<i32> = vec![17, 31, 73, 47, 23];
+    length_vector.extend(extension.iter().cloned());
+
     let mut hashlist: Vec<i32> = Vec::new();
     for i in 0..256 {hashlist.push(i)};
     let hash_len = hashlist.len() as i32;
 
-    for _ in 0..rounds {
+    for _ in 0..range {
         for element in &length_vector {
-            end = (position + *element - 1)%hash_len;
-            for i in 0..(*element-1)/2 {
+            //println!("{}", *element);
+            if (position + *element -1)%hash_len < 0 {
+                end = ((position + *element -1)%hash_len) + hash_len;
+            } else {
+                end = (position + *element - 1)%hash_len;
+            }
+            //println!("Indexing form {} to {}", position, end);
+            for i in 0..(*element)/2 {
                 temp_begin = (position + i)%hash_len;
                 if end - i < 0 {
                     temp_end = ((end - i)%hash_len) + hash_len;
@@ -55,6 +57,26 @@ fn hash(length_vector: Vec<i32>, rounds: i32) -> i32{
             skip_size += 1;
         }
     }
+    
+    let mut hash_digest: String = String::new();
+    let mut part_vec: Vec<i32> = Vec::new();
 
-    hashlist[0] * hashlist[1]
+    for i in 0..16 {
+        part_vec.clear();
+        for j in 16*i..16*(i+1) {
+            part_vec.push(hashlist[j]);
+        }
+        hash_digest.push_str(&xor_part(part_vec.to_vec()));
+    }
+
+    hash_digest
+}
+
+fn xor_part(part: Vec<i32>) -> String {
+    let mut re: i32 = part[0];
+    for i in 1..part.len() {
+        re ^= part[i];
+    }
+    //println!("{}", re);
+    format!("{:01$x}", re, 2)
 }
