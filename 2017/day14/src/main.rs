13ac85f7f77415cd596fec2f621bf1cddf81b1c9
Explorer001 <lukas.gehreke@gmail.com>
2017-12-14 19:36:56 +0100

    added part 2

--- 2017/day14/src/main.rs
+++ 2017/day14/src/main.rs
@@ -6,22 +6,100 @@ use regex::Regex;
 
 fn main() {
     let input = "wenycdww".to_string();
-    println!("{}", get_used_field_count(input));
+    //let test = "flqrgnkx".to_string();
+    println!("{:?}", get_frag_info(input));
 }
 
-fn get_used_field_count(hash: String) -> i32 {
+fn get_frag_info(hash: String) -> (i32, i32) {
     let mut re: i32 = 0;
     let mut hs = hash.to_string();
     let orig_hash = hash.to_string();
     let mut hash_bin: String;
+
+    let mut grid: Vec<Vec<i32>> = Vec::new();
+
     for i in 0..128 {
         hs.push('-');
         hs.push_str(&(i.to_string()));
         hash_bin = get_binary(knothash::hash(hs.to_string()));
+        grid.push(hash_bin.chars().map(|c| c.to_string().parse::<i32>().unwrap()).collect());
         re += hash_bin.chars().map(|c| c.to_string().parse::<i32>().unwrap()).sum::<i32>();
         hs = orig_hash.to_string();
     }
+    
+    let mut objectives: Vec<(usize,usize)> = Vec::new();
 
+    let x_max = grid[0].len()-1;
+    let y_max = grid.len()-1;
+    let mut removed_group: bool;
+    let mut group_count = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            removed_group = false;
+            if grid[i][j] != 0 {
+                removed_group = true;
+                grid[i][j] = 0;
+                objectives.extend(get_adjacent(i,j,y_max, x_max).iter());
+                while !objectives.is_empty() {
+                    let (y,x) = objectives.pop().unwrap();
+                    if grid[y][x] != 0 {
+                        grid[y][x] = 0;
+                        objectives.extend(get_adjacent(y,x,y_max, x_max).iter());
+                    }
+                }
+            }
+            if removed_group {
+                group_count += 1;
+            }
+        }
+    }
+
+    (re, group_count)
+}
+
+fn get_adjacent(y: usize, x: usize, y_max: usize, x_max: usize) -> Vec<(usize,usize)> {
+    let mut re: Vec<(usize,usize)> = Vec::new();
+    if y == 0 {
+        if x == 0 {
+            re.push((y+1,x));
+            re.push((y,x+1));
+        } else if x == x_max {
+            re.push((y,x-1));
+            re.push((y+1,x));
+        } else {
+            re.push((y+1,x));
+            re.push((y,x-1));
+            re.push((y,x+1));
+        }
+    } else if y == y_max {
+        if x == 0 {
+            re.push((y-1,x));
+            re.push((y,x+1));
+        } else if x == x_max {
+            re.push((y,x-1));
+            re.push((y-1,x));
+        } else {
+            re.push((y-1,x));
+            re.push((y,x-1));
+            re.push((y,x+1));
+        }
+    } else {
+	    if x == 0 {
+            re.push((y-1,x));
+            re.push((y+1,x));
+            re.push((y,x+1));
+        } else if x == x_max {
+            re.push((y-1,x));
+            re.push((y,x-1));
+            re.push((y+1,x));
+        } else {
+            re.push((y-1,x));
+            re.push((y+1,x));
+            re.push((y,x-1));
+            re.push((y,x+1));
+        }
+    }
     re
 }
 
