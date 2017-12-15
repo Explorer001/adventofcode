68f1a0a5613e5ce1f79d63a82e34b7c5ca1ee744
Explorer001 <lukas.gehreke@gmail.com>
2017-12-15 12:22:52 +0100

    added part 2

--- 2017/day15/src/main.rs
+++ 2017/day15/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    println!("Matches: {}", solve(512, 191, 40000000));
+    println!("Matches: {}", solve(512, 191, 40000000, false));
 }
 
-fn solve(seed_a: i64, seed_b: i64, gen_range: usize) -> i32{
+fn solve(seed_a: i64, seed_b: i64, gen_range: usize, part1: bool) -> i32{
     let mut matches: i32 = 0;
     
     let gen_a_factor: i64 = 16807;
     let gen_b_factor: i64 = 48271;
+
+    let criteria_a: i64 = 4;
+    let criteria_b: i64 = 8;
 
     let divider: i64 = 2147483647;
 
@@ -16,15 +19,47 @@ fn solve(seed_a: i64, seed_b: i64, gen_range: usize) -> i32{
     let mut bin_a: String;
     let mut bin_b: String;
 
-    for _ in 0..gen_range {
-        val_a = (val_a * gen_a_factor) % divider;
-        val_b = (val_b * gen_b_factor) % divider;
-        bin_a = get_lowest_16_binary_bits(val_a);
-        bin_b = get_lowest_16_binary_bits(val_b);
-        if bin_a == bin_b {
-            matches += 1;
+    let mut matching: i32 = 0;
+
+    if part1 {
+        for _ in 0..gen_range {
+            val_a = (val_a * gen_a_factor) % divider;
+            val_b = (val_b * gen_b_factor) % divider;
+            bin_a = get_lowest_16_binary_bits(val_a);
+            bin_b = get_lowest_16_binary_bits(val_b);
+            if bin_a == bin_b {
+                matches += 1;
+            }
+        }
+    } else {
+        let mut ok_a: bool;
+        let mut ok_b: bool;
+        while matching < 5000000 {
+            ok_a = false;
+            ok_b = false;
+            while !ok_a || !ok_b {
+                if !ok_a {
+                    val_a = (val_a * gen_a_factor) % divider;
+                    if val_a % criteria_a == 0 {
+                        ok_a = true;
+                    }
+                }
+                if !ok_b {
+                    val_b = (val_b * gen_b_factor) % divider;
+                    if val_b % criteria_b == 0 {
+                        ok_b = true;
+                    }
+                }
+            }
+            matching += 1;
+            bin_a = get_lowest_16_binary_bits(val_a);
+            bin_b = get_lowest_16_binary_bits(val_b);
+            if bin_a == bin_b {
+                matches += 1;
+            }
         }
     }
+
     matches
 }
 
